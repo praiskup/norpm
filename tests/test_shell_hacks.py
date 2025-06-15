be68017e57e69d962a3ad252e2ff34cfefae9f9e
Pavel Raiskup <pavel@raiskup.cz>
2025-06-15 14:36:13 +0200

    Work-around for %(echo %{gitcommit} | cut -c 1-8)

--- tests/test_shell_hacks.py
+++ tests/test_shell_hacks.py
@@ -1,6 +1,6 @@
 """ Test hacks norpm has to overcome common '%()' patterns """
 
-from norpm.specfile import specfile_expand
+from norpm.specfile import specfile_expand, SHELL_REGEXP_HACKS
 from norpm.macro import MacroRegistry
 
 def test_commit_shortener():
@@ -8,11 +8,28 @@ def test_commit_shortener():
     assert specfile_expand("""\
 %define foo e02feaaf245528401c40dfae113e3fc424b1deef
 %global short %(abc=%{foo} ; echo ${abc:0:7})
+%global short2 %(echo %{foo} | cut -c-3)
 %short
 %{sub %foo 2 3}%{sub %{foo} 3 2}
 %{sub %foo -4 -3}
+%short2
 """, MacroRegistry()) == """\
 e02feaa
 02
 de
+e02
 """
+
+
+def test_cut_hack():
+    """Some packages use cut"""
+    # $ diff -u /tmp/norpm /tmp/rpm  | grep ^- | grep cut | sed 's/[^%]*//' | sort -u
+    data = """\
+%(echo '%{commit}' | cut -b -7)
+%(echo %{gitcommit} | cut -c 1-8)
+%(echo %{git_commit} | cut -c -8)
+%(echo %{git_rev} | cut -c-8)
+%(echo "%{_git_rev}" | cut -c-8)
+"""
+    for macro in data.splitlines():
+        assert SHELL_REGEXP_HACKS[1]["regexp"].match(macro)
