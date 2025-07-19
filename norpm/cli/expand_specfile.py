9b5ad387949368d7e57dedfc7be49e11c972bb1f
Pavel Raiskup <pavel@raiskup.cz>
2025-07-19 02:24:13 +0200

    specfile: new helper for parsing specfile tags

--- norpm/cli/expand_specfile.py
+++ norpm/cli/expand_specfile.py
@@ -8,6 +8,19 @@ import argparse
 import sys
 from norpm.macrofile import system_macro_registry
 from norpm.specfile import specfile_expand, specfile_expand_string
+from norpm.specfile import ParserHooks
+
+
+class Hooks(ParserHooks):
+    """ Gather access to spec tags """
+    def __init__(self):
+        self.tags = {}
+    def tag_found(self, name, value, _tag_raw):
+        """ Gather EclusiveArch, ExcludeArch and BuildArch """
+        if name == "source":
+            name = "source0"
+        self.tags[name] = value
+
 
 def _get_parser():
     parser = argparse.ArgumentParser()
@@ -17,6 +30,8 @@ def _get_parser():
         "specfile to standard output though.  Expand additional string "
         "and print just that."
         ))
+    parser.add_argument("--get-tag", help=(
+        "Parse specfile and extract given tag"))
     return parser
 
 
@@ -27,13 +42,22 @@ def _main():
     registry["dist"] = ""
     registry.known_norpm_hacks()
     try:
+        hooks = Hooks()
         with open(opts.specfile, "r", encoding="utf8") as fd:
-            expanded_specfile = specfile_expand(fd.read(), registry)
+            expanded_specfile = specfile_expand(fd.read(), registry, hooks)
         if opts.expand_string:
             if opts.expand_string[-1] != "\n":
                 opts.expand_string += "\n"
             sys.stdout.write(specfile_expand_string(opts.expand_string, registry))
             return 0
+        if opts.get_tag:
+            tag = opts.get_tag.lower()
+            try:
+                sys.stdout.write(hooks.tags[tag] + "\n")
+                return 0
+            except KeyError:
+                sys.stderr.write(f"Tag {tag} not found\n")
+                return 1
         sys.stdout.write(expanded_specfile)
         return 0
     except RecursionError as exc:
