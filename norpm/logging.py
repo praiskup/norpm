7f6c29c8d16e70f654429cbb3b8ee3ba95c5eadf
Pavel Raiskup <pavel@raiskup.cz>
2025-10-14 12:23:04 +0200

    Do not modify default logger
    We, e.g., don't want to switch default log level to DEBUG, that would,
    e.g., cause unexpected logentries in rpmautospec testsuite:
    https://github.com/fedora-infra/rpmautospec/pull/319

--- norpm/logging.py
+++ norpm/logging.py
@@ -4,7 +4,7 @@ Logging configuration for norpm project.
 
 import logging
 
-def get_logger(name=None):
+def get_logger(name='norpm'):
     """Allocate configured logger."""
     log = logging.getLogger(name)
     log.setLevel(logging.DEBUG)
