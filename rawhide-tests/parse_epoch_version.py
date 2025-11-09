946016237867340a349203b3430519983d09555b
Pavel Raiskup <pavel@raiskup.cz>
2025-11-09 19:07:04 +0100

    tests: use hooks for reading tags (rawhide tests)
    Fixes: #41

--- rawhide-tests/parse_epoch_version.py
+++ rawhide-tests/parse_epoch_version.py
@@ -9,9 +9,10 @@ import copy
 import glob
 import os
 import sys
-from norpm.specfile import specfile_expand_string, specfile_expand
+from norpm.specfile import specfile_expand
 from norpm.macro import MacroRegistry
 from norpm.macrofile import system_macro_registry
+from norpm.specfile import ParserHooks
 
 db = MacroRegistry()
 db = system_macro_registry()
@@ -19,22 +20,35 @@ db.known_norpm_hacks()
 
 db["dist"] = ""
 
+
+class Hooks(ParserHooks):
+    """ Gather access to spec tags """
+    def __init__(self):
+        self.tags = {}
+    def tag_found(self, name, value, _tag_raw):
+        """ Gather EclusiveArch, ExcludeArch, BuildArch... """
+        if name in ["epoch", "version"]:
+            self.tags[name] = value
+
+
 for spec in sorted(glob.glob("/rpm-specs/*.spec")):
     sys.stderr.write(f"Doing {spec}\n")
     basename = os.path.basename(spec)
     try:
+        hooks = Hooks()
         with open(spec, "r", encoding="utf8") as fd:
             # we don't want to leak macros from one spec file to another
             temp_db = copy.deepcopy(db)
             try:
-                specfile_expand(fd.read(), temp_db)
+                specfile_expand(fd.read(), temp_db, hooks)
             except Exception:  # pylint: disable=broad-exception-caught
                 print(f"{basename}:Unexpected error")
                 continue
+
             epoch = "(none)"
-            if "epoch" in temp_db:
-                epoch = specfile_expand_string("%epoch", temp_db)
-            version = specfile_expand_string("%version", temp_db)
+            if "epoch" in hooks.tags:
+                epoch = hooks.tags["epoch"]
+            version = hooks.tags["version"]
             print(f"{basename}:{epoch}:{version}", flush=True)
     except RecursionError:
         print(f"{basename}: RecursionError", flush=True)
